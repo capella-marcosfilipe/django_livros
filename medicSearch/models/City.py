d55582e66f1965cec1f03d21e3b234f33198aae1
Marcos Filipe Capella <105896656+capella-marcosfilipe@users.noreply.github.com>
2024-08-12 17:44:15 +0000

    Migrated tables

--- medicSearch/models/City.py
+++ medicSearch/models/City.py
@@ -6,11 +6,11 @@ State (FK) and Name are required. Status is True by default.
 from medicSearch.models import *
 
 class City(models.Model):
-    state = models.ForeignKey(State, null=False, max_length=20, related_name='state', on_delete=models.SET_NULL)
+    state = models.ForeignKey(State, null=True, max_length=20, related_name='state', on_delete=models.SET_NULL)
     name = models.CharField(null=False, max_length=20)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return '{} - {}'.format(self.name, self.state.name)
+        return '{} - {}'.format(self.name, self.state.name) # type: ignore
--- medicSearch/models/__init__.py
+++ medicSearch/models/__init__.py
@@ -1,7 +1,6 @@
 '''
 This file communicates to Django what models should reflect on our database structure.
 '''
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -15,11 +14,11 @@ ROLE_CHOICE = (
 )
 
 # Models in use:
-from .Profile import Profile
-from .Speciality import Speciality
+from .Rating import Rating
 from .DayWeek import DayWeek
 from .State import State
 from .City import City
 from .Neighborhood import Neighborhood
 from .Address import Address
-from .Rating import Rating
+from .Speciality import Speciality
+from .Profile import Profile
