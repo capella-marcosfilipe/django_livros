7c31d2f3720483eaf53bb67a4087915cc86d78b0
Marcos Filipe Capella <105896656+capella-marcosfilipe@users.noreply.github.com>
2024-08-12 20:47:43 +0000

    Cleaned unused secret_key off main settings

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0uwl8q*jhmnot0m2sjyx)x%aw5u^uqqv9sdf!7@z_#m9%ivpvo"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
