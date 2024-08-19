6c99aac991197bd6ddabfebe10cbc8dc28d0ad3f
Marcos Filipe Capella <105896656+capella-marcosfilipe@users.noreply.github.com>
2024-08-19 14:42:07 +0000

    Created home with the form for searching medics

--- medicSearch/views/HomeView.py
+++ medicSearch/views/HomeView.py
@@ -1,7 +1,7 @@
 '''
 This view will let us search for doctors based on address and speciality.
 '''
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>', status=200)
+    return render(request, template_name='home/home.html', status=200)
